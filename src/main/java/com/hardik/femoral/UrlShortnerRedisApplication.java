5afadb0450d47a7fc832e51896c6f258bd7c37cb
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-05-20 18:41:05 +0530

    added API's

--- src/main/java/com/hardik/femoral/UrlShortnerRedisApplication.java
+++ src/main/java/com/hardik/femoral/UrlShortnerRedisApplication.java
@@ -1,7 +1,10 @@
 package com.hardik.femoral;
 
+import org.apache.commons.validator.routines.UrlValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @SpringBootApplication
 public class UrlShortnerRedisApplication {
@@ -10,4 +13,9 @@ public class UrlShortnerRedisApplication {
 		SpringApplication.run(UrlShortnerRedisApplication.class, args);
 	}
 
+	@Bean
+	@Primary
+	public UrlValidator urlValidator() {
+		return new UrlValidator(new String[] { "http", "https" });
+	}
 }
